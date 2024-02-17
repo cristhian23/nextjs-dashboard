1ec1058afea2d2308176ac6bacc7f2386539133a
Cristhian Cuevas <cristhian.cuevas@ab-inbev.com>
2024-02-17 18:54:40 -0400

    chore: add lusitana

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,7 @@
-import {Inter, Acme} from 'next/font/google';
+import {Inter, Lusitana } from 'next/font/google';
 
-export const acme= Inter({ subsets: ['latin'] });
 export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+    });
