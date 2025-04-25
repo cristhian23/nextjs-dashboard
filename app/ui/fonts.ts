196c5da46f76f636da742195f2dc44462473ae64
Cristhian Cuevas <cristhian.cuevas@ab-inbev.com>
2025-04-25 18:22:51 -0400

    chore: resolve errors

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,4 @@
-import { Lusitana, Montserrat } from 'next/font/google';
-
-export const montserrat = Montserrat({
-  subsets: ['latin']
-});
+import { Lusitana } from 'next/font/google';
 
 export const lusitana = Lusitana({
     subsets: ['latin'],
